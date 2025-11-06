7bfaac28c04e2f095b87c4e5cacd38834b907fb2
Braden Bledsoe <batfat00@byu.edu>
2025-11-05 23:35:04 -0700

    formatting

--- src/metrics.js
+++ src/metrics.js
@@ -88,7 +88,6 @@ function getMemoryUsagePercentage() {
     const totalMemory = os.totalmem();
     const freeMemory = os.freemem();
     const usedMemory = totalMemory - freeMemory;
-    const memoryUsage = (usedMemory / totalMemory) * 100;
     return Number(((usedMemory / totalMemory) * 100).toFixed(2));
 }
 
